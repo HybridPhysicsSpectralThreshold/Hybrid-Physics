f7167d9637c5873eda7c327bcb97517e65fedff9
HybridPhysicsSpectralThreshold <Hybrid_physic@outlook.com>
2026-03-13 03:43:13 +0200

    Create __init__.py

---
+++ hpst /__init__.py
@@ -0,0 +1,10 @@
+"""HPST: Hybrid Physics-Spectral-Threshold framework."""
+
+from hpst.expr import *
+from hpst.theorem import *
+from hpst.system import FormalSystem
+from hpst.data import DataLoader
+from hpst.results import ExperimentResults, ResultsSaver
+from hpst.experiment import run_experiment
+
+__version__ = "0.1.0"
